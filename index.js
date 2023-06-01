52202e0da0718435ec61e77f91fed656b8565ec8
tytech039 <tywilson2009@gmail.com>
2023-06-01 17:17:03 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 80;
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
